9ab342aa97268e5b14a87a579132c0e0dee2d913
Sardiniangale <sardiniangale24@gmail.com>
2023-07-31 14:21:45 +1200

    Ver 0.6

--- procedural-caves/godot-project/KinematicBody2D.gd
+++ procedural-caves/godot-project/KinematicBody2D.gd
@@ -24,4 +24,17 @@ func _physics_process(delta: float) -> void:
 
 var is_falling := _velocity.y > 0.0 and not is_on_floor()
 var is_jumping := Input.is_action_just_pressed("jump") and is_on_floor()
-var is_double_jumping :=
+var is_double_jumping := Input.is_action_just_pressed("jump") and is_falling
+var is_jump_cancelled := Input.is_action_just_released("jump") and _velocity.y < 0.0
+var is_idling := is_on_floor() and is_zero_approx(_velocity.x)
+var is_running := is_on_floor() and not is_zero_approx(_velocity.x)
+
+if is_jumping:
+	_jumps_made += 1
+	_velocity.y = -jump_strength
+elif is_double_jumping:
+	_jumps_made += 1
+	if _jump_made <= maximum_jumps:
+		_velocity.y = -double_jump_strength
+
+_velocity = move_and_slide(_velocity, UP_DIRECTION)
